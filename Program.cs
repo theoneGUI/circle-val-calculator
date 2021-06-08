00f4a1646922c619d8d5daf2d2ddc8f315744808
Aidan Wright <49991562+theoneGUI@users.noreply.github.com>
2021-06-08 11:09:07 -0500

    Adding file initally

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CsharpProj1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Circle Value Calculator\n\n");
+            calc();
+
+        }
+        static void calc()
+        {
+            try
+            {
+                string piQ = piQuery();
+                double pi = Math.PI;
+                if (piQ == "" || piQ.ToLower() == "n")
+                {
+                    pi = Math.PI;
+                }
+                else if (piQ.ToLower() == "y")
+                {
+                    pi = 3.14;
+                }
+                else
+                {
+                    Console.WriteLine("Please type Y or N");
+                    piQuery();
+                }
+                Console.Write("Input radius here: ");
+                string input = Console.ReadLine();
+                double radius = double.Parse(input);
+                double diameter = radius * 2;
+                Console.WriteLine($"Circumference: {pi * diameter} units\nDiameter: {diameter} units\nArea: {pi * (radius * radius)} units\n\n");
+                Console.Write("Press enter to quit");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please input a number");
+                calc();
+            }
+        }
+        static string piQuery()
+        {
+            Console.Write("Use 3.14 for pi?(y/N) ");
+            string piQ = Console.ReadLine();
+            return piQ;
+        }
+    }
+   
+}
